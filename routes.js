a94ff7a7b450e2a9a3da16a9eb0d515f7aeb9b34
Kate Molony <katemolony96@gmail.com>
2023-07-05 15:06:57 +0100

    first commit

---
+++ routes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { dashboardController } from "./controllers/dashboard-controller.js";
+import { aboutController } from "./controllers/about-controller.js";
+import { playlistController } from "./controllers/playlist-controller.js";
+
+export const router = express.Router();
+
+router.get("/", dashboardController.index);
+router.get("/dashboard", dashboardController.index);
+router.post("/dashboard/addplaylist", dashboardController.addPlaylist);
+router.get("/about", aboutController.index);
+router.get("/playlist/:id", playlistController.index);
+
+router.post("/playlist/:id/addtrack", playlistController.addTrack);
